172206fb1b22c4808c4464e5f2d77fb033f727ab
divyajayan <divya.jayan@quantiguous.com>
2017-10-26 10:59:25 +0530

    helper function for decrypt_encrypted_field

--- lib/encrypted_field.rb
+++ lib/encrypted_field.rb
@@ -1,5 +1,6 @@
 require "encrypted_field/version"
 require 'encrypted_field/controller_additions'
+require 'encrypted_field/decrypt_password_field'
 require "encrypted_field/railtie" if defined?(Rails)
 require "openssl"
 
