5bc0ce2b8b953fc65cb10b4027d5a727b4ac9f28
divyajayan <divya.jayan@quantiguous.com>
2017-11-02 18:30:33 +0530

    encrypted_field.js to support rails 4

--- lib/encrypted_field/version.rb
+++ lib/encrypted_field/version.rb
@@ -1,3 +1,3 @@
 module EncryptedField
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
--- vendor/assets/encrypted_field.js
+++ vendor/assets/encrypted_field.js
@@ -13,3 +13,20 @@ $(document).on('turbolinks:load', function(){
     })
   });
 });
+
+// Rails 4, without TurboLinks
+$(document).on('ready page:load', function(){
+  if ( typeof(Turbolinks) == 'undefined' ) {
+    $('form').submit(function( event ) {
+      var encrypt = new JSEncrypt();
+      $('[data-encrypt]').each(function(){
+        unencrypted = $(this);
+        encrypt.setKey($('#public_key').val());
+        encrypted = encrypt.encrypt(unencrypted.val());
+        if (encrypted != false) {
+          unencrypted.val(encrypted);
+        }  
+      })
+    });
+  }
+});
